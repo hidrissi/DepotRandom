8c1da9c469444248b87b0e969aa09eb3b6f3253a
hidrissi <59539833+hidrissi@users.noreply.github.com>
2022-01-19 13:47:04 +0000

    Add files via upload

---
+++ DepotRandom/random/src/main/java/random/main.java
@@ -0,0 +1,16 @@
+package random;
+
+import java.util.ArrayList;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(" Tester le projet Random");
+		randomTab.randomTable();
+		randomTab.afficherTable();
+		
+	}
+
+}
---
+++ DepotRandom/random/src/main/java/random/randomTab.java
@@ -0,0 +1,39 @@
+package random;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class randomTab {
+
+	static ArrayList<Integer> res = new ArrayList<Integer>();
+	
+	public static void randomTable() {
+		
+		int i =0;
+		while(i<1000000) {
+			Random rand = new Random();
+			res.add(rand.nextInt());
+			
+			i++;
+		}
+	}
+	
+	
+public static void afficherTable() {
+	    System.out.println("Contenu Tableau ");
+		for(int i =0; i<res.size(); i++) {
+			System.out.println(res.get(i));
+		}
+		
+		 System.out.println("Fin du Tableau \n");
+
+	}
+
+public static void afficherTab() {
+    System.out.println("Contenu Tableau ");
+	for(int i =0; i<res.size(); i++) {
+		System.out.println(res.get(i));
+	}
+}
+
+}
